e6289ca32b57ba4a59ca9c12683dc54345c2059a
Giannis <96373640+gyfantidis@users.noreply.github.com>
2023-01-13 23:53:26 +0200

    First Commit

--- Project1/src/App.js
+++ Project1/src/App.js
@@ -4,9 +4,10 @@ import Expenses from "./components/Expenses/Expenses";
 
 const App = () => {
   const expenses = [
-    { title: "Car Insuranse", amount: 3000.33, date: new Date(2022, 12, 22) },
+    { title: "Car Insuranse", amount: 3000.33, date: new Date(2023, 1, 1) },
     { title: "Car Insuranse2", amount: 31000.33, date: new Date(2022, 11, 22) },
     { title: "Car Insurans3", amount: 30.33, date: new Date(2022, 10, 22) },
+    { title: "Car Insurans4", amount: 12.3, date: new Date(2021, 5, 2) }
   ];
 
   return (
