782fa2bb4634dac343431b5a40df88b7e9447290
Giannis <96373640+gyfantidis@users.noreply.github.com>
2023-02-02 13:37:14 +0200

    First Commit

--- Project1/src/App.js
+++ Project1/src/App.js
@@ -4,10 +4,14 @@ import NewExpense from "./components/NewExpense/NewExpense";
 import Expenses from "./components/Expenses/Expenses";
 
 const myExpenses = [
-  { id:'a1', title: "Car Insuranse", amount: 3000.33, date: new Date(2023, 1, 1) },
-  { id:'a2', title: "Car Insuranse2", amount: 31000.33, date: new Date(2022, 11, 22) },
-  { id:'a3', title: "Car Insurans3", amount: 30.33, date: new Date(2022, 10, 22) },
-  { id:'a4', title: "Car Insurans4", amount: 12.3, date: new Date(2021, 5, 2) }
+  { id:'a1', title: "Super Market", amount: 150.33, date: new Date(2020, 1, 1) },
+  { id:'a2', title: "Fuel oil", amount: 80, date: new Date(2020, 11, 22) },
+  { id:'a3', title: "Drinks", amount: 30.33, date: new Date(2022, 10, 22) },
+  { id:'a4', title: "Cigarettes", amount: 12.3, date: new Date(2023, 5, 2) },
+  { id:'a5', title: "Coffee", amount: 2.3, date: new Date(2023, 1, 2) },
+  { id:'a6', title: "Transportation", amount: 2.3, date: new Date(2020, 5, 2) },
+  { id:'a7', title: "Food", amount: 11.2, date: new Date(2020, 4, 2) },
+  { id:'a8', title: "Pet Food", amount: 20, date: new Date(2020, 4, 22) }
 ];
 
 
